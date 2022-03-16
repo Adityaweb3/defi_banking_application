d42f889e04182a527c4e9e0d024bd3cfac2b1981
Adityaweb3 <ap7596112@gmail.com>
2022-03-16 18:52:12 +0530

    full stack blockchain application

---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+const { modules } = require('web3/src');
+
+require('babel-register');
+require('babel-polyfill');
+
+
+modules.exports = {
+    networks : {
+        development : {
+            host : '127.0.0.1:7545' ,
+            port : '7545' ,
+            network_id : '*' //connect to any network
+        },
+    },
+    contract_directory : './src/contracts/' ,
+    contracts_build_directory : './src/truffle_abis',
+    compilers : {
+        solc : {
+            version : '^0.5.0' ,
+            optimizer : {
+                enabled : true,
+                runs : 200,
+            },
+             
+        },
+    }
+
+
+}
